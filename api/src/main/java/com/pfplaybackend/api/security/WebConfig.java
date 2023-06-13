34b5c9f5cd07a2648d3026bf76de77c6a9951826
withcolinsong <withcolinsong@gmail.com>
2023-06-13 20:45:06 +0900

    feat: docker compose 분리 및 cors 추가

--- api/src/main/java/com/pfplaybackend/api/security/WebConfig.java
+++ api/src/main/java/com/pfplaybackend/api/security/WebConfig.java
@@ -1,12 +1,10 @@
 package com.pfplaybackend.api.security;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Slf4j
 @Configuration
 public class WebConfig {
 
