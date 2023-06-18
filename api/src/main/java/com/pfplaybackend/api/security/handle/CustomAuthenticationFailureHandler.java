072cde92df4f7e2e322eeefb79da70a10566fd6f
withcolinsong <withcolinsong@gmail.com>
2023-06-18 18:33:05 +0900

    feat: logging

--- api/src/main/java/com/pfplaybackend/api/security/handle/CustomAuthenticationFailureHandler.java
+++ api/src/main/java/com/pfplaybackend/api/security/handle/CustomAuthenticationFailureHandler.java
@@ -3,12 +3,14 @@ package com.pfplaybackend.api.security.handle;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
@@ -16,6 +18,8 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("response info={}", response);
+        log.info("request info={}", request);
         response.sendRedirect(DOMAIN + "/login");
     }
 }
