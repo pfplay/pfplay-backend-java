b6ab39bc8d5b43737db680cb231391bc02cb1d0b
withcolinsong <withcolinsong@gmail.com>
2023-07-09 19:45:34 +0900

    feat: 인증 이후 dummy api 추가

---
+++ api/src/main/java/com/pfplaybackend/api/user/dto/DummyResponse.java
@@ -0,0 +1,19 @@
+package com.pfplaybackend.api.user.dto;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collection;
+@Getter
+public class DummyResponse {
+
+    private final String email;
+    private final String testSnakeCase = null;
+    private final Collection<GrantedAuthority> authorities;
+
+    public DummyResponse(JwtAuthenticationToken token) {
+        this.email = token.getToken().getClaims().get("iss").toString();
+        this.authorities = token.getAuthorities();
+    }
+}
