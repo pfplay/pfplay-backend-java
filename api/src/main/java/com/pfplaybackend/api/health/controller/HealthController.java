20411cd8f01bc85f1a54c81f3e8900fd722c8c90
withcolinsong <withcolinsong@gmail.com>
2023-08-07 22:18:20 +0900

    feat: swagger setting 및 common header,api 네이밍 수정

--- api/src/main/java/com/pfplaybackend/api/health/controller/HealthController.java
+++ api/src/main/java/com/pfplaybackend/api/health/controller/HealthController.java
@@ -1,23 +1,14 @@
 package com.pfplaybackend.api.health.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<?> health() {
         return ResponseEntity.ok("success");
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<?> index() {
-        return ResponseEntity.ok("welcome");
     }
 
 }
--- api/src/main/java/com/pfplaybackend/api/security/WebConfig.java
+++ api/src/main/java/com/pfplaybackend/api/security/WebConfig.java
@@ -1,26 +1,21 @@
 package com.pfplaybackend.api.security;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+
+
 
 @Slf4j
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://pfplay.io")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .maxAge(3600); // 프리플라이트 요청의 유효 기간
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://pfplay.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600); // 프리플라이트 요청의 유효 기간
     }
-
 }
