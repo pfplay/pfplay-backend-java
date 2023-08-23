54a65f5676fcb023168595d9612fdbf604c8592f
withcolinsong <withcolinsong@gmail.com>
2023-08-23 23:58:17 +0900

    feat : global exception

---
+++ api/src/main/java/com/pfplaybackend/api/common/ExceptionResult.java
@@ -0,0 +1,20 @@
+package com.pfplaybackend.api.common;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Schema(description = "Exception Result")
+@Getter
+@Builder
+@AllArgsConstructor
+public class ExceptionResult {
+    @Schema(implementation = HttpStatus.class)
+    private HttpStatus status;
+    private int code;
+    private String message;
+    private String stackTrace;
+}
