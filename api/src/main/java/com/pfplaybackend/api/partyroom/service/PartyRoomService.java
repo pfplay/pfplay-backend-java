bd2a9282f44720a124e1a93f0bee3e46f10a1dbd
withcolinsong <withcolinsong@gmail.com>
2023-08-23 23:59:22 +0900

    feat : global exception 처리를 위해 try catch 제거 및 리팩토링

--- api/src/main/java/com/pfplaybackend/api/partyroom/service/PartyRoomService.java
+++ api/src/main/java/com/pfplaybackend/api/partyroom/service/PartyRoomService.java
@@ -3,7 +3,6 @@ package com.pfplaybackend.api.partyroom.service;
 import com.pfplaybackend.api.entity.PartyRoom;
 import com.pfplaybackend.api.partyroom.presentation.dto.PartyRoomCreateDto;
 import com.pfplaybackend.api.partyroom.repository.PartyRoomRepository;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +17,7 @@ public class PartyRoomService {
 
     @Transactional
     public PartyRoom createPartyRoom(PartyRoomCreateDto dto) {
-        try {
-            return partyRoomRepository.save(dto.toEntity());
-        } catch (DuplicateKeyException e) {
-            throw new DuplicateKeyException("Party room name is Duplicate", e);
-        }
+        return partyRoomRepository.save(dto.toEntity());
     }
 
 }
