cb912f1fdd325216cd7a68ebef16dbbbca6a3f88
kimhyunrip <steve@blockodyssey.io>
2023-10-21 02:22:59 +0900

    feat: 플레이리스트 추가 시 개수 제한 예외처리

--- api/src/main/java/com/pfplaybackend/api/playlist/repository/PlayListRepository.java
+++ api/src/main/java/com/pfplaybackend/api/playlist/repository/PlayListRepository.java
@@ -1,13 +1,15 @@
 package com.pfplaybackend.api.playlist.repository;
 
 import com.pfplaybackend.api.entity.PlayList;
+import com.pfplaybackend.api.playlist.enums.PlayListType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PlayListRepository extends JpaRepository<PlayList, Long> {
+    // 내 플레이리스트, 그랩리스트 전체 조회
     List<PlayList> findByUserIdOrderByTypeDescOrderNumberAsc(Long userId);
 
-    Optional<PlayList> findTopByUserIdOrderByOrderNumberDesc(Long userId);
+    // 내 플레이리스트 정렬하여 조회
+    List<PlayList> findByUserIdAndTypeOrderByOrderNumberDesc(Long userId, PlayListType type);
 }
