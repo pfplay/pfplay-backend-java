4650ff70449634f6c0a85f50f2dd757d5ad27bde
kimhyunrip <steve@blockodyssey.io>
2023-10-23 21:38:06 +0900

    mod: 아바타 리스트 조회 swagger 업데이트(type enum):

--- api/src/main/java/com/pfplaybackend/api/avatar/service/AvatarService.java
+++ api/src/main/java/com/pfplaybackend/api/avatar/service/AvatarService.java
@@ -3,9 +3,9 @@ package com.pfplaybackend.api.avatar.service;
 import com.pfplaybackend.api.avatar.presentation.dto.AvatarBodyDto;
 import com.pfplaybackend.api.avatar.repository.AvatarRepository;
 import com.pfplaybackend.api.entity.Avatar;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +16,19 @@ public class AvatarService {
         this.avatarRepository = avatarRepository;
     }
 
-    public  List<AvatarBodyDto> getAvatarBodies() {
-        return avatarRepository.findBy(AvatarBodyDto.class);
+    public List<AvatarBodyDto> getAvatarBodies() {
+        List<Avatar> result = avatarRepository.findAll();
+        List<AvatarBodyDto> dtoList = new ArrayList<>();
+        for (Avatar avatar : result) {
+            AvatarBodyDto dto = AvatarBodyDto.builder()
+                    .id(avatar.getId())
+                    .type(avatar.getType())
+                    .name(avatar.getName())
+                    .image(avatar.getImage())
+                    .point(avatar.getPoint())
+                    .build();
+            dtoList.add(dto);
+        }
+        return dtoList;
     }
 }
