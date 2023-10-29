bb1252447944011a58f52c2843db0d0173d863ba
withcolinsong <withcolinsong@gmail.com>
2023-10-29 09:56:15 +0900

    feat : User jwt 정보 분리

--- api/src/main/java/com/pfplaybackend/api/common/JwtTokenInfo.java
+++ api/src/main/java/com/pfplaybackend/api/common/JwtTokenInfo.java
@@ -1,24 +1,74 @@
 package com.pfplaybackend.api.common;
 
+import com.pfplaybackend.api.entity.User;
+import com.pfplaybackend.api.enums.Authority;
+import com.pfplaybackend.api.partyroom.presentation.dto.UserDto;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Collection;
+import java.util.Objects;
 
 @Getter
-public class JwtTokenInfo {
+@Slf4j
+public class JwtTokenInfo implements Authentication {
 
     private final JwtAuthenticationToken token;
     private final Collection<GrantedAuthority> authorities;
     private final String email;
     private final Long userId;
+    private final User user;
 
-    public JwtTokenInfo(Authentication authentication) {
+    @Builder
+    public JwtTokenInfo(Authentication authentication, User user) {
         this.token = (JwtAuthenticationToken) authentication;
-        this.email = token.getToken().getClaims().get("iss").toString();
         this.userId = Long.parseLong(token.getToken().getClaims().get("userId").toString());
+        this.email = token.getToken().getClaims().get("iss").toString();
         this.authorities = token.getAuthorities();
+        this.user = user;
+    }
+
+    public boolean isUser() {
+        return !Objects.isNull(user);
+    }
+
+    public boolean isGuest() {
+        return this.authorities.stream().anyMatch(
+                o-> o.getAuthority().equals(Authority.ROLE_GUEST.getRole())
+        );
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return this;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.user;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        this.isAuthenticated();
+    }
+
+    @Override
+    public String getName() {
+        return this.user.getNickname();
     }
 }
