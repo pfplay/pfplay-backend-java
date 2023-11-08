72c79fde2752d873734af8a3f34782ffdf1b11ed
withColinSong <71534090+withColinSong@users.noreply.github.com>
2023-11-08 22:01:07 +0900

    플레이 리스트 audit 미적용 모듈 수정 (#33)

--- api/src/main/java/com/pfplaybackend/api/entity/MusicList.java
+++ api/src/main/java/com/pfplaybackend/api/entity/MusicList.java
@@ -1,5 +1,6 @@
 package com.pfplaybackend.api.entity;
 
+import com.pfplaybackend.api.entity.audit.BaseTime;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,17 +8,18 @@ import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import java.time.LocalDateTime;
 
 @Getter
 @DynamicInsert
 @DynamicUpdate
-@Table(name = "MUSIC_LIST",
+@Table(
+        name = "MUSIC_LIST",
         indexes = {
-                @Index(name = "music_list_play_list_id_IDX", columnList = "play_list_id")
-        })
+            @Index(name = "music_list_play_list_id_IDX", columnList = "play_list_id")
+        }
+)
 @Entity
-public class MusicList {
+public class MusicList extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "integer unsigned")
@@ -40,21 +42,14 @@ public class MusicList {
     @Comment("url")
     private String url;
 
-    @Column(columnDefinition = "datetime default current_timestamp")
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
-
-    protected MusicList() {
-    }
+    protected MusicList() { }
 
     @Builder
-    public MusicList(PlayList playList, Long orderNumber, String name, String duration, String url, LocalDateTime updatedAt) {
+    public MusicList(PlayList playList, Long orderNumber, String name, String duration, String url) {
         this.playList = playList;
         this.orderNumber = orderNumber;
         this.name = name;
         this.duration = duration;
         this.url = url;
-        this.updatedAt = updatedAt;
     }
 }
