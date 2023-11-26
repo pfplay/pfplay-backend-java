819dd51f484c86d1d2a4906840beff0e2e602e8f
withcolinsong <withcolinsong@gmail.com>
2023-11-26 22:27:48 +0900

    feat :  회원 프로필 업데이트 시 닉네임 유니크 조건 추가 (게스트는 제외)
    - 프로필 업데이트 유저 롤 추가
    - 200명 파티룸 정원 초과 exception 추가

--- api/src/main/java/com/pfplaybackend/api/user/controller/UserSignController.java
+++ api/src/main/java/com/pfplaybackend/api/user/controller/UserSignController.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -82,6 +83,7 @@ public class UserSignController {
                     description = "프로필 설정 실패"
             )
     })
+    @Secured("ROLE_USER")
     @PatchMapping("/profile")
     public ResponseEntity<?> userProfile(
             @Valid @RequestBody ProfileUpdateRequest request
--- api/src/main/java/com/pfplaybackend/api/user/repository/UserRepository.java
+++ api/src/main/java/com/pfplaybackend/api/user/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.pfplaybackend.api.user.repository;
 import com.pfplaybackend.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    User findByNickname(String nickname);
 
 }
