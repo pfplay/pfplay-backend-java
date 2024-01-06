0588d8c7c3f50ebc8a505fe1f3ce2507ee69b83c
steve <sylvius4@gmail.com>
2024-01-06 21:02:56 +0900

    mod: swagger - set profile

--- api/src/main/java/com/pfplaybackend/api/user/controller/UserSignController.java
+++ api/src/main/java/com/pfplaybackend/api/user/controller/UserSignController.java
@@ -17,6 +17,7 @@ import com.pfplaybackend.api.user.service.CustomUserDetailService;
 import com.pfplaybackend.api.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -82,10 +83,20 @@ public class UserSignController {
     @Operation(summary = "유저 마이 프로필 설정")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
-                    description = "프로필 설정 성공"
+                    description = "프로필 설정 성공",
+                    content = @Content(mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = "OK"
+                    ))
+            ),
+            @ApiResponse(responseCode = "409", description = "닉네임 중복",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\": \"이미 존재하는 값입니다.\"}"
+                            ))
             ),
             @ApiResponse(responseCode = "500",
-                    description = "프로필 설정 실패"
+                    description = "알 수 없는 에러입니다."
             )
     })
     @Secured("ROLE_USER")
@@ -93,13 +104,9 @@ public class UserSignController {
     public ResponseEntity<?> setUserProfile(
             @Valid @RequestBody ProfileUpdateRequest request
     ) {
-        try {
-            JwtTokenInfo user = customUserDetailService.getUserDetails(SecurityContextHolder.getContext().getAuthentication());
-            userService.setProfile(user.getUser(), request);
-            return ResponseEntity.ok().body(ApiCommonResponse.success("OK"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+        JwtTokenInfo user = customUserDetailService.getUserDetails(SecurityContextHolder.getContext().getAuthentication());
+        userService.setProfile(user.getUser(), request);
+        return ResponseEntity.ok().body(ApiCommonResponse.success("OK"));
     }
 
     @Operation(summary = "유저 마이 프로필 조회")
