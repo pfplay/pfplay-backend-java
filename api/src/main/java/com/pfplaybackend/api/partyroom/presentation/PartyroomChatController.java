63f5cd6558b7158ca29ba315eca78346d0d274fb
Kyooong <shawn11305@gmail.com>
2024-06-20 00:48:32 +0900

    change name socket to chat

--- api/src/main/java/com/pfplaybackend/api/partyroom/presentation/PartyroomChatController.java
+++ api/src/main/java/com/pfplaybackend/api/partyroom/presentation/PartyroomChatController.java
@@ -3,31 +3,26 @@ package com.pfplaybackend.api.partyroom.presentation;
 import com.pfplaybackend.api.common.ApiCommonResponse;
 import com.pfplaybackend.api.config.jwt.dto.UserCredentials;
 import com.pfplaybackend.api.config.oauth2.dto.CustomAuthentication;
-import com.pfplaybackend.api.partyroom.application.PartyroomSocketService;
+import com.pfplaybackend.api.partyroom.application.PartyroomChatService;
 
 import com.pfplaybackend.api.partyroom.exception.InvalidPartyroomIdRequestException;
 import com.pfplaybackend.api.partyroom.exception.UnsupportedSocketRequestException;
 import com.pfplaybackend.api.partyroom.presentation.api.PartyroomChatApi;
 import com.pfplaybackend.api.partyroom.presentation.dto.ChatDto;
-import com.pfplaybackend.api.partyroom.presentation.dto.PenaltyDto;
-import com.pfplaybackend.api.partyroom.presentation.dto.PromoteDto;
-import com.pfplaybackend.api.user.domain.model.domain.User;
 import com.pfplaybackend.api.user.domain.model.value.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
-public class PartyroomSocketController implements PartyroomChatApi {
-    private final PartyroomSocketService partyroomSocketService;
+public class PartyroomChatController implements PartyroomChatApi {
+    private final PartyroomChatService partyroomSocketService;
 
     @MessageMapping("/partyroom/api/v1/send/message")
     public ResponseEntity<?> sendMessage(MessageHeaders headers, ChatDto chatDto) {
@@ -41,26 +36,6 @@ public class PartyroomSocketController implements PartyroomChatApi {
                  throw new InvalidPartyroomIdRequestException("Invalid Partyroom Id");
             }
             partyroomSocketService.sendMessage(chatDto);
-            return ResponseEntity.ok(ApiCommonResponse.success("OK"));
-        } catch (UnsupportedSocketRequestException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-
-    @MessageMapping("/partyroom/api/v1/send/penalty")
-    public ResponseEntity<?> sendPenalty(PenaltyDto penaltyDto)  {
-        try {
-            partyroomSocketService.sendPenalty(penaltyDto);
-            return ResponseEntity.ok(ApiCommonResponse.success("OK"));
-        } catch (UnsupportedSocketRequestException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-
-    @MessageMapping("/partyroom/api/v1/send/promote")
-    public ResponseEntity<?> sendPromote(PromoteDto promoteDto)  {
-        try {
-            partyroomSocketService.sendPromote(promoteDto);
             return ResponseEntity.ok(ApiCommonResponse.success("OK"));
         } catch (UnsupportedSocketRequestException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
