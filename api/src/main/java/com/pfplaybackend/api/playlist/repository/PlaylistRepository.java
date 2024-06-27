6463629b211f7e6447c72e0e94580776f0ddd5a6
JeekLee <j3kkun@gmail.com>
2024-06-27 22:43:45 +0900

    feat: Controller, Service, Repository 구현

--- api/src/main/java/com/pfplaybackend/api/playlist/repository/PlaylistRepository.java
+++ api/src/main/java/com/pfplaybackend/api/playlist/repository/PlaylistRepository.java
@@ -1,6 +1,5 @@
 package com.pfplaybackend.api.playlist.repository;
 
-import com.pfplaybackend.api.playlist.domain.entity.domainmodel.Playlist;
 import com.pfplaybackend.api.playlist.domain.entity.data.PlaylistData;
 import com.pfplaybackend.api.playlist.domain.enums.PlaylistType;
 import com.pfplaybackend.api.playlist.repository.custom.PlaylistRepositoryCustom;
@@ -8,8 +7,10 @@ import com.pfplaybackend.api.user.domain.value.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlaylistRepository extends JpaRepository<PlaylistData, Long>, PlaylistRepositoryCustom {
     List<PlaylistData> findAllByOwnerId(UserId userId);
     List<PlaylistData> findByOwnerIdAndTypeOrderByOrderNumberDesc(UserId userId, PlaylistType type);
+    Optional<PlaylistData> findByIdAndOwnerIdAndType(Long playlistId, UserId userId, PlaylistType type);
 }
