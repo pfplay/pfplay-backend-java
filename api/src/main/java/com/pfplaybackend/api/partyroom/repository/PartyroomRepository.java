5a76957f88e8740b0d490b1cc801d7577f3c2564
Kyooong <shawn11305@gmail.com>
2024-08-25 01:54:12 +0900

    implements session saving in redis

--- api/src/main/java/com/pfplaybackend/api/partyroom/repository/PartyroomRepository.java
+++ api/src/main/java/com/pfplaybackend/api/partyroom/repository/PartyroomRepository.java
@@ -1,7 +1,6 @@
 package com.pfplaybackend.api.partyroom.repository;
 
 import com.pfplaybackend.api.partyroom.domain.entity.data.PartyroomData;
-import com.pfplaybackend.api.partyroom.domain.value.PartyroomId;
 import com.pfplaybackend.api.partyroom.repository.custom.PartyroomRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
