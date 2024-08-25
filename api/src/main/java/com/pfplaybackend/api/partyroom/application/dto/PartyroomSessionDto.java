6c89e35924aaf0ab2cfbd6ded182c556d899e6eb
Kyooong <shawn11305@gmail.com>
2024-08-26 01:16:37 +0900

    fixed, data entity to dto partyroom session dto

--- api/src/main/java/com/pfplaybackend/api/partyroom/application/dto/PartyroomSessionDto.java
+++ api/src/main/java/com/pfplaybackend/api/partyroom/application/dto/PartyroomSessionDto.java
@@ -1,4 +1,4 @@
-package com.pfplaybackend.api.partyroom.domain.entity.data;
+package com.pfplaybackend.api.partyroom.application.dto;
 
 import com.pfplaybackend.api.partyroom.domain.value.PartyroomId;
 import com.pfplaybackend.api.user.domain.value.UserId;
@@ -9,23 +9,26 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-public class PartyroomSessionData {
+public class PartyroomSessionDto {
     private String sessionId;
     private UserId userId;
     private PartyroomId partyroomId;
+    private long memberId;
 
     @Builder
-    private PartyroomSessionData(String sessionId, UserId userId, PartyroomId partyroomId) {
+    private PartyroomSessionDto(String sessionId, UserId userId, PartyroomId partyroomId, long memberId) {
         this.sessionId = sessionId;
         this.userId = userId;
         this.partyroomId = partyroomId;
+        this.memberId = memberId;
     }
 
-    public static PartyroomSessionData create(String sessionId, UserId userId, PartyroomId partyroomId) {
-        return PartyroomSessionData.builder()
+    public static PartyroomSessionDto create(String sessionId, UserId userId, PartyroomId partyroomId, long memberId) {
+        return PartyroomSessionDto.builder()
                 .sessionId(sessionId)
                 .userId(userId)
                 .partyroomId(partyroomId)
+                .memberId(memberId)
                 .build();
     }
 }
