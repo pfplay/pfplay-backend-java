28cf70ee6f315242cc5eb267e5a569c0daf7047d
Kyooong <shawn11305@gmail.com>
2024-08-25 23:33:53 +0900

    change session handler name to session manager

--- api/src/main/java/com/pfplaybackend/api/config/websocket/event/handler/SessionCacheManager.java
+++ api/src/main/java/com/pfplaybackend/api/config/websocket/event/handler/SessionCacheManager.java
@@ -1,7 +1,7 @@
 package com.pfplaybackend.api.config.websocket.event.handler;
 import com.pfplaybackend.api.user.domain.value.UserId;
 
-public interface SessionEventHandler {
+public interface SessionCacheManager {
     void saveSessionCache(String sessionId, UserId userId, String destination);
     void deleteSessionCache(String sessionId);
 }
