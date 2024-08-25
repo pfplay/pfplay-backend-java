402b253b2f5bd3f200d3317e34c228a9f6b5e811
Kyooong <shawn11305@gmail.com>
2024-08-25 19:26:31 +0900

    implements delete session

--- api/src/main/java/com/pfplaybackend/api/config/websocket/event/UnsubscriptionEventListener.java
+++ api/src/main/java/com/pfplaybackend/api/config/websocket/event/UnsubscriptionEventListener.java
@@ -1,5 +1,9 @@
 package com.pfplaybackend.api.config.websocket.event;
 
+import com.pfplaybackend.api.common.exception.ExceptionCreator;
+import com.pfplaybackend.api.config.websocket.event.handler.SessionEventHandler;
+import com.pfplaybackend.api.config.websocket.exception.SessionException;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -7,14 +11,25 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
+import java.security.Principal;
+
 @Component
+@RequiredArgsConstructor
 public class UnsubscriptionEventListener implements ApplicationListener<SessionUnsubscribeEvent> {
     private final static Logger logger = LoggerFactory.getLogger(UnsubscriptionEventListener.class);
+    private final SessionEventHandler sessionEventHandler;
 
     @Override
     public void onApplicationEvent(SessionUnsubscribeEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
-        logger.info("Web socket connection closed: " + sessionId);
+        Principal principal = headerAccessor.getUser();
+        if (principal == null) {
+            logger.warn("Unauthorized session requested, UserId is null" + " Session ID: " + sessionId);
+            throw ExceptionCreator.create(SessionException.UNAUTHORIZED_SESSION);
+        }
+        sessionEventHandler.deleteSessionCache(sessionId);
+
+        logger.info("Session has unsubscribed, sessionId : " + sessionId);
     }
 }
--- api/src/main/java/com/pfplaybackend/api/config/websocket/event/handler/SessionEventHandler.java
+++ api/src/main/java/com/pfplaybackend/api/config/websocket/event/handler/SessionEventHandler.java
@@ -3,4 +3,5 @@ import com.pfplaybackend.api.user.domain.value.UserId;
 
 public interface SessionEventHandler {
     void saveSessionCache(String sessionId, UserId userId, String destination);
+    void deleteSessionCache(String sessionId);
 }
