e77d13e4191e7ae0f03dca8c77520ac31ad22236
livinglikekrillin <livinglikekrillin@gmail.com>
2024-09-02 01:02:56 +0900

    Reactoring reaction logic

--- api/src/main/java/com/pfplaybackend/api/config/websocket/event/listener/DisconnectionEventListener.java
+++ api/src/main/java/com/pfplaybackend/api/config/websocket/event/listener/DisconnectionEventListener.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.security.Principal;
@@ -24,11 +25,32 @@ public class DisconnectionEventListener implements ApplicationListener<SessionDi
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
         Principal principal = headerAccessor.getUser();
-        if (principal == null) {
-            logger.warn("Unauthorized session requested, UserId is null" + " Session ID: " + sessionId);
-            throw ExceptionCreator.create(SessionException.UNAUTHORIZED_SESSION);
-        }
         sessionCacheManager.deleteSessionCache(sessionId);
         logger.info("Web socket connection closed: " + sessionId);
+
+        CloseStatus closeStatus = event.getCloseStatus();
+        Integer closeStatusCode = closeStatus.getCode();
+        logger.info(closeStatus.getReason());
+
+        switch (closeStatusCode) {
+            case 1000:
+                logger.info("WebSocket session {} was closed normally (CLOSE_NORMAL).", sessionId);
+                break;
+            case 1001:
+                logger.info("WebSocket session {} was closed because the client is going away (CLOSE_GOING_AWAY).", sessionId);
+                break;
+            case 1002:
+                logger.warn("WebSocket session {} was closed due to protocol error (CLOSE_PROTOCOL_ERROR).", sessionId);
+                break;
+            case 1003:
+                logger.warn("WebSocket session {} was closed due to unacceptable data (CLOSE_NOT_ACCEPTABLE).", sessionId);
+                break;
+            case 1006:
+                logger.error("WebSocket session {} was closed abnormally (CLOSE_ABNORMAL).", sessionId);
+                break;
+            default:
+                logger.warn("WebSocket session {} was closed with an unexpected status code: {}", sessionId, closeStatusCode);
+                break;
+        }
     }
 }
