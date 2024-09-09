ac33bcf59d66d98237b0a2a66ebd9af6d237caf3
JeekLee <j3kkun@gmail.com>
2024-09-09 18:23:52 +0900

    feat: Jwt 인증 관련 Exception 정의

---
+++ api/src/main/java/com/pfplaybackend/api/config/jwt/exception/JwtAuthenticationException.java
@@ -0,0 +1,22 @@
+package com.pfplaybackend.api.config.jwt.exception;
+
+import com.pfplaybackend.api.common.exception.SecurityException;
+import com.pfplaybackend.api.common.exception.http.UnauthorizedException;
+import lombok.Getter;
+
+@Getter
+public enum JwtAuthenticationException implements SecurityException {
+    ACCESS_TOKEN_NOT_FOUND("JWT-001", "Json Web Token not found", UnauthorizedException.class),
+    ACCESS_TOKEN_INVALID("JWT-001", "Json Web Token is not valid", UnauthorizedException.class),
+    ACCESS_TOKEN_EXPIRED("JWT-002", "Json Web Token is expired", UnauthorizedException.class);
+
+    private final String errorCode;
+    private final String message;
+    private final Class<?> aClass;
+
+    JwtAuthenticationException(String errorCode, String message, Class<?> aClass) {
+        this.errorCode = errorCode;
+        this.message = message;
+        this.aClass = aClass;
+    }
+}
