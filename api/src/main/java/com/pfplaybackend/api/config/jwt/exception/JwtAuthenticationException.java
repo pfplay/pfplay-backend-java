63adef19412dc68be6478cbcdda5287c914b7c0d
JeekLee <j3kkun@gmail.com>
2024-09-09 19:02:45 +0900

    fix: modify errorCode

--- api/src/main/java/com/pfplaybackend/api/config/jwt/exception/JwtAuthenticationException.java
+++ api/src/main/java/com/pfplaybackend/api/config/jwt/exception/JwtAuthenticationException.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 public enum JwtAuthenticationException implements SecurityException {
     ACCESS_TOKEN_NOT_FOUND("JWT-001", "Json Web Token not found", UnauthorizedException.class),
-    ACCESS_TOKEN_INVALID("JWT-001", "Json Web Token is not valid", UnauthorizedException.class),
-    ACCESS_TOKEN_EXPIRED("JWT-002", "Json Web Token is expired", UnauthorizedException.class);
+    ACCESS_TOKEN_INVALID("JWT-002", "Json Web Token is not valid", UnauthorizedException.class),
+    ACCESS_TOKEN_EXPIRED("JWT-003", "Json Web Token is expired", UnauthorizedException.class);
 
     private final String errorCode;
     private final String message;
